0adc0e798d8ee464de24e179e261f393ce9debdb
Akshay Gulabrao <oxtookbronze@gmail.com>
2020-11-22 13:24:17 -0800

    Added rubric

--- main.c
+++ main.c
@@ -77,6 +77,18 @@ TickFct_OnOff_Ctrl() {
 
          Actuator = calcAct;
          ActualPrev = Actual;
+         /*
+         PID control is common in many embedded systems, like keeping speed constant in an RC car, keeping a motor spinning at a steady rate, keeping a liquid's temperature steady at a desired rate, keeping a flying drone steady, etc. For this project, you can choose from two options. Both are simulations. Embedded programmers commonly make simulations to ensure their systems will work.
+
+        Create a PID simulator in RIMS. The P, I, and D values would all be hardcoded in the C code. A0 controls whether to use pid control (0) or manual control (1). For manual control, A6 is fan speed up and A7 is fan speed down (if both pressed, ignore both). B represents the ball's height, with the target being 100 (as in the web PID simulator). The above is worth 95/100 -- a strong A. For the last 5 points, let A1 control the output mode, with 0 being as above, but 1 meaning the 8 LEDs visually depict the balls height, with the goal to have the ball steady at B3.
+
+        You can use the P, I, and D constants used in the zyBook's simulator: Kp 0.05, Ki 0.001, Kd 2.5
+
+        Submit your code so TAs can see it; you'll get 0 of 1 points but that is irrelevant.
+
+        To ensure you are starting early and developing incrementally, you should submit your code AFTER EVERY 30 MINUTES OF WORK or more often if desired. Just copy-paste, click Submit, and then keep working, so we can see a history of your work, including dates/times and the code itself.
+        /*
+
          break;
       default: // ADD default behaviour below
       break;
